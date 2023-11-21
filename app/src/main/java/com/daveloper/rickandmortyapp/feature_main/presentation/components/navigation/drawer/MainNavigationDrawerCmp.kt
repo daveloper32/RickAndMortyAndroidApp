8053824d1333ee7c6fd3525af0fc8a5c9420cef5
David Hernández <daves98hg@hotmail.com>
2023-11-21 15:24:25 -0500

    Solve navigation drawer size on screen rotated

--- app/src/main/java/com/daveloper/rickandmortyapp/feature_main/presentation/components/navigation/drawer/MainNavigationDrawerCmp.kt
+++ app/src/main/java/com/daveloper/rickandmortyapp/feature_main/presentation/components/navigation/drawer/MainNavigationDrawerCmp.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -48,15 +47,10 @@ fun MainNavigationDrawer(
     content: @Composable () -> Unit
 ) {
     ModalNavigationDrawer(
+        modifier = modifier,
         drawerState = drawerState,
         drawerContent = {
-            ModalDrawerSheet(
-                modifier = modifier
-                    .fillMaxSize()
-                    .padding(
-                        end = 64.dp
-                    )
-            ) {
+            ModalDrawerSheet {
                 Box {
                     IconButton(
                         modifier = Modifier
