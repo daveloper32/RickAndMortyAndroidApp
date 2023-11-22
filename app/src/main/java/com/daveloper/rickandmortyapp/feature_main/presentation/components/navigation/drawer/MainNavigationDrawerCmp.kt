e00948db29a500761fba0d29552129964d7b8248
David Hernández <daves98hg@hotmail.com>
2023-11-21 22:04:32 -0500

    Refactor navigation drawer max width

--- app/src/main/java/com/daveloper/rickandmortyapp/feature_main/presentation/components/navigation/drawer/MainNavigationDrawerCmp.kt
+++ app/src/main/java/com/daveloper/rickandmortyapp/feature_main/presentation/components/navigation/drawer/MainNavigationDrawerCmp.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -62,7 +61,7 @@ fun MainNavigationDrawer(
             ) {
                 LazyColumn(
                     modifier = Modifier
-                        .fillMaxWidth(0.8f)
+                        .fillMaxWidth(0.85f)
                         .fillMaxHeight(),
                     verticalArrangement = Arrangement.SpaceBetween
                 ) {
